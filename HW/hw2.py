974194c9f3954b70e85c4251d8171ad969b176cc
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2023-04-09 19:27:00 +0200

    hw2 done

--- HW/hw2.py
+++ HW/hw2.py
@@ -1,9 +1,25 @@
+
+import math
+
+# NOTE This is the Hoeffding example (task1)
+
+print("Hoeffding example:")
+
+N = 100.0
+E_in = 0.1
+E_out = 0.2
+e = abs(E_in-E_out)
+
+bound = 2*math.exp(-2*math.pow(e,2)*N)
+
+print("		the bound is {}".format(bound))
+
 print("Nonlinear Logistic Regression")
-print("		or polynomial (degree=2) transform")
+print("		or polynomial (degree=2,3) transform")
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import math
+from sklearn.linear_model import LogisticRegression
 
 def train_test_split(data,labels,p = 0.5):
 	p = int(p*len(labels))
@@ -13,58 +29,15 @@ def train_test_split(data,labels,p = 0.5):
 	test_labels = labels[p-1:-1]
 	return train_data,test_data,train_labels,test_labels
 
-sigmoid = lambda x : 1 / (1 + math.exp(-x))
-
-
-class LogisticRegression():
-	def __init__(self):
-		self.w_hat = None
-		self.bias = 1
-		self.learning_rate = 0.01
-	def fit(self,data,labels,max_iterations=500):
-		self.w_hat = np.zeros(data.shape[1])
-		batch_size = 8
-		N = len(labels)
-		for epoch in range(max_iterations):
-			#print("epoch",epoch)
-			for b in range(int(N/batch_size)+1):
-				mini_data = data[b*batch_size:min(batch_size*(b+1),N)]
-				mini_labels = labels[b*batch_size:min(batch_size*(b+1),N)]
-				
-				#print(data[b*batch_size:min(batch_size*(b+1),N)])
-				#print("		b",b)
-				mini_predict = self.predict(mini_data)
-				mini_gradient,bias_error = self.error_gradient(mini_data,mini_labels,mini_predict)
-				self.w_hat = self.w_hat - self.learning_rate * mini_gradient
-				self.bias = self.bias - self.learning_rate * bias_error
-				#print(mini_gradient)
-			if epoch % 100 == 0:
-				print(self.accuracy(labels,self.predict(data)))
-
-	def sigmoid(self,data):
-		return 1 / (1 + np.exp(data))
-	def binary_cross_entropy(self,true,prediction):
-		N = len(true)
-		sum = 0
-		for i in range(N):
-			sum += true[i] * math.log2(prediction[i]) + (1-true) * math.log2(1 - prediction[i])
-		return -1/N * sum
-	def error_gradient(self,data,true,prediction):
-		return np.matmul(  np.transpose(data)  ,( prediction - true )) , sum( prediction - true )
-
-		pass
-	def predict(self,data):
-		return np.asarray(self.sigmoid([ np.dot(self.w_hat,data[i]) + self.bias for i in range(len(data))]))
-		#self.binary_cross_entropy(np.dot(self.w_hat,data) + self.bias)
-	def accuracy(self,true,prediction):
-		good_count = 0.0
-		for i in range(len(true)):
-			if true[i] == prediction[i]:
-				good_count+=1
-			else:
-				pass
-				#print(true[i],prediction[i])
-		return good_count / len(true)
+def accuracy(true,prediction):
+	good_count = 0.0
+	for i in range(len(true)):
+		if true[i] == prediction[i]:
+			good_count+=1
+		else:
+			pass
+			#print(true[i],prediction[i])
+	return good_count / len(true)
 
 	
 
@@ -90,32 +63,86 @@ labels[labels>1]=1
 labels=labels.astype(int)
 
 data=data.values[:,:-1]
+#data=data[:,[0,4]]
+
 
 #Standardize data (substract mean divide with std)
-#data= (data - np.mean(data)) / np.std(data)
+data= (data - np.mean(data)) / np.std(data)
 
 
 
 Tdata = PolyTransform(data,degree = 2)
 
-
 #print(Tdata)
 #quit()
 
-train_data,test_data,train_labels,test_labels = train_test_split(Tdata,labels,0.8)
-#train_data,train_labels = (data,labels)
-#test_data,test_labels = (data,labels)
-lr=LogisticRegression()
-lr.fit(train_data,train_labels,10000)
-prediction_in=lr.predict(train_data)
-prediction_out=lr.predict(test_data)
-print("---")
-print(lr.accuracy(train_labels,prediction_in))
-print(lr.accuracy(test_labels,prediction_out))
+# NOTE: So this transform does not help. Am I made a mistake (probably, how should i know).
+# How is this should work? When was this coverd on the lecture?
+
+print("Fit with regular data:")
+train_data,test_data,train_labels,test_labels = train_test_split(data,labels,0.5)
+clf = LogisticRegression(random_state=1,max_iter=1000).fit(train_data, train_labels)
+print("In error:",clf.score(train_data, train_labels))
+print("Out error:",clf.score(test_data, test_labels))
+
+# So this is worse.... :(
+print("Fit with Transformed data:")
+train_data,test_data,train_labels,test_labels = train_test_split(Tdata,labels,0.5)
+clf = LogisticRegression(random_state=1,max_iter=1000).fit(train_data, train_labels)
+print("In error:",clf.score(train_data, train_labels))
+print("Out error:",clf.score(test_data, test_labels))
+
+
+#task 3
+# NOTE: Please explaine me how is this task make any sense?
+def visualize(data,labels,a,b):
+	plt.scatter(data[:,0],data[:,1],c=labels)
+	xx = np.linspace(min(data[:,0]) , max(data[:,0]))
+	yy = a * xx + b
+	plt.plot(xx, yy, 'k-')
 
 
 
-#print(Tdata)
+from sklearn.inspection import DecisionBoundaryDisplay
+import matplotlib.pyplot as plt
+
+data=data[:,[0,4]]
+Tdata = PolyTransform(data,degree = 3)
+clf = LogisticRegression(random_state=1,max_iter=1000).fit(Tdata, labels)
+#print(clf.)
+
+w = clf.coef_
+a = w[0,0] / w[0,1]
+b =  3.5 #hack
+visualize(data,labels,a,b)
+
+#_, ax = plt.subplots(figsize=(4, 3))
+#DecisionBoundaryDisplay.from_estimator(
+#    clf,
+#    Tdata,
+#    cmap=plt.cm.Paired,
+#    ax=ax,
+#    response_method="predict",
+#    plot_method="pcolormesh",
+#    shading="auto",
+#    eps=0.5,
+#    n_features = 6,
+#)
+
+# Plot also the training points
+#plt.scatter(Tdata[:, 0], Tdata[:, 1], c=labels, edgecolors="k", cmap=plt.cm.Paired)
+
+#NOTE: I hate machine learning, and this is why.
+
+
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+
+
+
+
 
 quit()
 
